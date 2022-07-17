88446cf02fed7c3eea57ace4209d331148148a7c
cabraldasilvac <wcabraldasilvac@gmail.com>
2022-07-17 12:30:37 -0300

    renamed fields

--- registration.py
+++ registration.py
@@ -61,7 +61,6 @@ def setph(word,num):
     if num ==8:
         ph8.set(word)
 
-
 def read():
     conn = connection()
     cursor = conn.cursor()
@@ -72,23 +71,23 @@ def read():
     return results
 
 def add():
-    codigo = str(codigoEntry.get())
-    data_inicio = str(data_inicioEntry.get())
-    nome_projeto = str(nome_projetoEntry.get())
-    tipo = str(tipoEntry.get())
-    techs = str(techsEntry.get())
+    code = str(codeEntry.get())
+    start_date = str(start_dateEntry.get())
+    project_name = str(project_nameEntry.get())
+    project_type = str(project_typeEntry.get())
+    technologies = str(technologiesEntry.get())
     status_ = str(status_Entry.get())
-    notas = str(notasEntry.get())
-    data_conclusao = str(data_conclusaoEntry.get())
+    annotations = str(annotationsEntry.get())
+    completion_date = str(completion_dateEntry.get())
 
-    if (data_inicio == "" or data_inicio == " ") or (nome_projeto == "" or nome_projeto == " ") or (tipo == "" or tipo == " ") or (techs == "" or techs == " ")or (status_ == "" or status_ == " ")or (notas == "" or notas == " ")or (data_conclusao == "" or data_conclusao == " "):
+    if (start_date == "" or start_date == " ") or (project_name == "" or project_name == " ") or (project_type == "" or project_type == " ") or (technologies == "" or technologies == " ")or (status_ == "" or status_ == " ")or (annotations == "" or annotations == " ")or (completion_date == "" or completion_date == " "):
         messagebox.showinfo("Error", "Please fill up the blank entry")
         return
     else:
         try:
             conn = connection()
             cursor = conn.cursor()
-            cursor.execute("INSERT INTO registration VALUES ('"+codigo+"','"+data_inicio+"','"+nome_projeto+"','"+tipo+"','"+techs+"','"+status_+"','"+notas+"','"+data_conclusao+"')")
+            cursor.execute("INSERT INTO registration VALUES ('"+code+"','"+start_date+"','"+project_name+"','"+project_type+"','"+technologies+"','"+status_+"','"+annotations+"','"+completion_date+"')")
             conn.commit()
             conn.close()
         except:
@@ -124,7 +123,7 @@ def delete():
         try:
             conn = connection()
             cursor = conn.cursor()
-            cursor.execute("DELETE FROM registration WHERE codigo='"+str(deleteData)+"'")
+            cursor.execute("DELETE FROM registration WHERE code='"+str(deleteData)+"'")
             conn.commit()
             conn.close()
         except:
@@ -136,53 +135,53 @@ def delete():
 def select():
     try:
         selected_item = my_tree.selection()[0]
-        codigo = str(my_tree.item(selected_item)['values'][0])
-        data_inicio = str(my_tree.item(selected_item)['values'][1])
-        nome_projeto = str(my_tree.item(selected_item)['values'][2])
-        tipo = str(my_tree.item(selected_item)['values'][3])
-        techs = str(my_tree.item(selected_item)['values'][4])
+        code = str(my_tree.item(selected_item)['values'][0])
+        start_date = str(my_tree.item(selected_item)['values'][1])
+        project_name = str(my_tree.item(selected_item)['values'][2])
+        project_type = str(my_tree.item(selected_item)['values'][3])
+        technologies = str(my_tree.item(selected_item)['values'][4])
         status_ = str(my_tree.item(selected_item)['values'][5])
-        notas = str(my_tree.item(selected_item)['values'][6])
-        data_conclusao = str(my_tree.item(selected_item)['values'][7])
+        annotations = str(my_tree.item(selected_item)['values'][6])
+        completion_date = str(my_tree.item(selected_item)['values'][7])
         
-        setph(codigo,1)
-        setph(data_inicio,2)
-        setph(nome_projeto,3)
-        setph(tipo,4)
-        setph(techs,5)
+        setph(code,1)
+        setph(start_date,2)
+        setph(project_name,3)
+        setph(project_type,4)
+        setph(technologies,5)
         setph(status_,6)
-        setph(notas,7)
-        setph(data_conclusao,8)
+        setph(annotations,7)
+        setph(completion_date,8)
 
     except:
         messagebox.showinfo("Error", "Please select a data row")
 
 def search():
-    codigo = str(codigoEntry.get())
-    data_inicio = str(data_inicioEntry.get())
-    nome_projeto = str(nome_projetoEntry.get())
-    tipo = str(tipoEntry.get())
-    techs = str(techsEntry.get())
+    code = str(codeEntry.get())
+    start_date = str(start_dateEntry.get())
+    project_name = str(project_nameEntry.get())
+    project_type = str(project_typeEntry.get())
+    technologies = str(technologiesEntry.get())
     status_ = str(status_Entry.get())
-    notas = str(notasEntry.get())
-    data_conclusao = str(data_conclusaoEntry.get())
+    annotations = str(annotationsEntry.get())
+    completion_date = str(completion_dateEntry.get())
 
     conn = connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM registration WHERE codigo='"+
-    codigo+"' or data_inicio='"+
-    data_inicio+"' or nome_projeto='"+
-    nome_projeto+"' or tipo='"+
-    tipo+"' or techs='"+
-    techs+"' or status_='"+
-    status_+"' or notas='"+
-    notas+"' or data_conclusao='"+
-    data_conclusao+"' ")
+    cursor.execute("SELECT * FROM registration WHERE code='"+
+    code+"' or start_date='"+
+    start_date+"' or project_name='"+
+    project_name+"' or project_type='"+
+    project_type+"' or technologies='"+
+    technologies+"' or status_='"+
+    status_+"' or annotations='"+
+    annotations+"' or completion_date='"+
+    completion_date+"' ")
     
     try:
         result = cursor.fetchall()
 
-        for num in range(0,9):
+        for num in range(0,8):
             setph(result[0][num],(num+1))
 
         conn.commit()
@@ -199,31 +198,31 @@ def update():
     except:
         messagebox.showinfo("Error", "Please select a data row")
 
-    codigo = str(codigoEntry.get())
-    data_inicio = str(data_inicioEntry.get())
-    nome_projeto = str(nome_projetoEntry.get())
-    tipo = str(tipoEntry.get())
-    techs = str(techsEntry.get())
+    code = str(codeEntry.get())
+    start_date = str(start_dateEntry.get())
+    project_name = str(project_nameEntry.get())
+    project_type = str(project_typeEntry.get())
+    technologies = str(technologiesEntry.get())
     status_ = str(status_Entry.get())
-    notas = str(notasEntry.get())
-    data_conclusao = str(data_conclusaoEntry.get())
+    annotations = str(annotationsEntry.get())
+    completion_date = str(completion_dateEntry.get())
 
-    if (data_inicio == "" or data_inicio == " ") or (nome_projeto == "" or nome_projeto == " ") or (tipo == "" or tipo == " ") or (techs == "" or techs == " ")or (status_ == "" or status_ == " ")or (notas == "" or notas == " ")or (data_conclusao == "" or data_conclusao == " "):
+    if (start_date == "" or start_date == " ") or (project_name == "" or project_name == " ") or (project_type == "" or project_type == " ") or (technologies == "" or technologies == " ")or (status_ == "" or status_ == " ")or (annotations == "" or annotations == " ")or (completion_date == "" or completion_date == " "):
         messagebox.showinfo("Error", "Please fill up the blank entry")
         return
     else:
         try:
             conn = connection()
             cursor = conn.cursor()
-            cursor.execute("SELECT * FROM registration WHERE  codigo='"+
-            codigo+"' or data_inicio='"+
-            data_inicio+"' or nome_projeto='"+
-            nome_projeto+"' or tipo='"+
-            tipo+"' or techs='"+
-            techs+"' or status_='"+
-            status_+"' or notas='"+
-            notas+"' or data_conclusao='"+
-            data_conclusao+"'")
+            cursor.execute("SELECT * FROM registration WHERE  code='"+
+            code+"' or start_date='"+
+            start_date+"' or project_name='"+
+            project_name+"' or project_type='"+
+            project_type+"' or technologies='"+
+            technologies+"' or status_='"+
+            status_+"' or annotations='"+
+            annotations+"' or completion_date='"+
+            completion_date+"'")
             conn.commit()
             conn.close()
         except:
@@ -235,42 +234,42 @@ def update():
 label = Label(root, text=" Project Registration System ", font=('Arial Bold', 35))
 label.grid(row=0, column=0, columnspan=8, rowspan=2, padx=50, pady=40)
 
-codigoLabel = Label(root, text="codigo", font=('Arial', 20))
-data_inicioLabel = Label(root, text="data_incio", font=('Arial', 20))
-nome_projetoLabel = Label(root, text="nome_projeto", font=('Arial', 20))
-tipoLabel = Label(root, text="tipo", font=('Arial', 20))
-techsLabel = Label(root, text="techs", font=('Arial', 20))
-status_Label = Label(root, text="status_", font=('Arial', 20))
-notasLabel = Label(root, text="notas", font=('Arial', 20))
-data_conclusaoLabel = Label(root, text="data_conclusao", font=('Arial', 20))
+codeLabel = Label(root, text="Code", font=('Arial', 20))
+start_dateLabel = Label(root, text="Start Date", font=('Arial', 20))
+project_nameLabel = Label(root, text="Project Name", font=('Arial', 20))
+project_typeLabel = Label(root, text="Type", font=('Arial', 20))
+technologiesLabel = Label(root, text="Technologies", font=('Arial', 20))
+status_Label = Label(root, text="Status", font=('Arial', 20))
+annotationsLabel = Label(root, text="Annotations", font=('Arial', 20))
+completion_dateLabel = Label(root, text="Date Conclusion", font=('Arial', 20))
 
 
-codigoLabel.grid(row=4, column=0, columnspan=1, padx=50, pady=5)
-data_inicioLabel.grid(row=5, column=0, columnspan=1, padx=50, pady=5)
-nome_projetoLabel.grid(row=6, column=0, columnspan=1, padx=50, pady=5)
-tipoLabel.grid(row=7, column=0, columnspan=1, padx=50, pady=5)
-techsLabel.grid(row=8, column=0, columnspan=1, padx=50, pady=5)
+codeLabel.grid(row=4, column=0, columnspan=1, padx=50, pady=5)
+start_dateLabel.grid(row=5, column=0, columnspan=1, padx=50, pady=5)
+project_nameLabel.grid(row=6, column=0, columnspan=1, padx=50, pady=5)
+project_typeLabel.grid(row=7, column=0, columnspan=1, padx=50, pady=5)
+technologiesLabel.grid(row=8, column=0, columnspan=1, padx=50, pady=5)
 status_Label.grid(row=9, column=0, columnspan=1, padx=50, pady=5)
-notasLabel.grid(row=10, column=0, columnspan=1, padx=50, pady=5)
-data_conclusaoLabel.grid(row=11, column=0, columnspan=1, padx=50, pady=5)
+annotationsLabel.grid(row=10, column=0, columnspan=1, padx=50, pady=5)
+completion_dateLabel.grid(row=11, column=0, columnspan=1, padx=50, pady=5)
 
-codigoEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph1)
-data_inicioEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph2)
-nome_projetoEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph3)
-tipoEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph4)
-techsEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph5)
+codeEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph1)
+start_dateEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph2)
+project_nameEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph3)
+project_typeEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph4)
+technologiesEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph5)
 status_Entry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph6)
-notasEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph7)
-data_conclusaoEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable =ph8)
+annotationsEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable = ph7)
+completion_dateEntry = Entry(root, width=55, bd=5, font=('Arial', 15), textvariable =ph8)
 
-codigoEntry.grid(row=4, column=1, columnspan=4, padx=5, pady=0)
-data_inicioEntry.grid(row=5, column=1, columnspan=4, padx=5, pady=0)
-nome_projetoEntry.grid(row=6, column=1, columnspan=4, padx=5, pady=0)
-tipoEntry.grid(row=7, column=1, columnspan=4, padx=5, pady=0)
-techsEntry.grid(row=8, column=1, columnspan=4, padx=5, pady=0)
+codeEntry.grid(row=4, column=1, columnspan=4, padx=5, pady=0)
+start_dateEntry.grid(row=5, column=1, columnspan=4, padx=5, pady=0)
+project_nameEntry.grid(row=6, column=1, columnspan=4, padx=5, pady=0)
+project_typeEntry.grid(row=7, column=1, columnspan=4, padx=5, pady=0)
+technologiesEntry.grid(row=8, column=1, columnspan=4, padx=5, pady=0)
 status_Entry.grid(row=9, column=1, columnspan=4, padx=5, pady=0)
-notasEntry.grid(row=10, column=1, columnspan=4, padx=5, pady=0)
-data_conclusaoEntry.grid(row=11, column=1, columnspan=4, padx=5, pady=0)
+annotationsEntry.grid(row=10, column=1, columnspan=4, padx=5, pady=0)
+completion_dateEntry.grid(row=11, column=1, columnspan=4, padx=5, pady=0)
 
 addBtn = Button(
     root, text="Add", padx=65, pady=25, width=10,
@@ -285,8 +284,8 @@ searchBtn = Button(
     root, text="Search", padx=65, pady=25, width=10,
     bd=5, font=('Arial', 18), bg="#F4FE82", command=search)
 resetBtn = Button(
-   # root, text="Reset", padx=65, pady=25, width=10,
-    #bd=5, font=('Arial', 18), bg="#F398FF", command=reset)
+    root, text="Reset", padx=65, pady=25, width=10,
+    bd=5, font=('Arial', 18), bg="#F398FF", command=reset)
 selectBtn = Button(
     root, text="Select", padx=65, pady=25, width=10,
     bd=5, font=('Arial', 18), bg="#EEEEEE", command=select)
@@ -301,26 +300,26 @@ selectBtn.grid(row=11, column=5, columnspan=1, rowspan=2)
 style = ttk.Style()
 style.configure("Treeview.Heading", font=('Arial Bold', 18))
 
-my_tree['columns'] = ("codigo","data_inicio","nome_projeto","tipo","techs","status_","notas","data_conclusao")
+my_tree['columns'] = ("code","start_date","project_name","project_type","technologies","status_","annotations","completion_date")
 
 my_tree.column("#0", width=0, stretch=NO)
-my_tree.column("codigo", anchor=W, width=100)
-my_tree.column("data_inicio", anchor=W, width=150)
-my_tree.column("nome_projeto", anchor=W, width=300)
-my_tree.column("tipo", anchor=W, width=165)
-my_tree.column("techs", anchor=W, width=150)
+my_tree.column("code", anchor=W, width=100)
+my_tree.column("start_date", anchor=W, width=150)
+my_tree.column("project_name", anchor=W, width=300)
+my_tree.column("project_type", anchor=W, width=165)
+my_tree.column("technologies", anchor=W, width=150)
 my_tree.column("status_", anchor=W, width=150)
-my_tree.column("notas", anchor=W, width=150)
-my_tree.column("data_conclusao", anchor=W, width=150)
+my_tree.column("annotations", anchor=W, width=150)
+my_tree.column("completion_date", anchor=W, width=150)
 
-my_tree.heading("codigo", text="Codigo", anchor=W)
-my_tree.heading("data_inicio", text="Data Inicio", anchor=W)
-my_tree.heading("nome_projeto", text="Nome Projeto", anchor=W)
-my_tree.heading("tipo", text="Tipo", anchor=W)
-my_tree.heading("techs", text="Techs", anchor=W)
-my_tree.heading("status_", text="Status_", anchor=W)
-my_tree.heading("notas", text="Notas", anchor=W)
-my_tree.heading("data_conclusao", text="Data Conclusao", anchor=W)
+my_tree.heading("code", text="Code", anchor=W)
+my_tree.heading("start_date", text="Start Date", anchor=W)
+my_tree.heading("project_name", text="Project Name", anchor=W)
+my_tree.heading("project_type", text="Type", anchor=W)
+my_tree.heading("technologies", text="Technologies", anchor=W)
+my_tree.heading("status_", text="Status", anchor=W)
+my_tree.heading("annotations", text="Annotations", anchor=W)
+my_tree.heading("completion_date", text="Date Conclusion", anchor=W)
 
 refreshTable()
 
